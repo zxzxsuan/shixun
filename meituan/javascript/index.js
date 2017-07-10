ae89bd300b970ebf406e61582ce73d6ce1d98bd6
zxzxsuan <mysm7813>
2017-07-10 16:52:15 +0800

    7.10

--- meituan/javascript/index.js
+++ meituan/javascript/index.js
@@ -1,31 +1,46 @@
-/**
- * Created by xiecheng on 2017/7/6.
- */
-$(function(){//文档就绪函数
+
+$(document).on("pageinit",function(event){
     var $guideUl = $("#guide ul");
     var $navSpan = $("#nav span");
     //click tap
     $navSpan.on("tap", function(){
-        $(this).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: - $(this).index() * $guideUl.width() / 2
-        });
+        swipeGuide($(this).index());
     });
     $guideUl.on("swipeleft", function(){
-        $navSpan.eq(1).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: -$guideUl.width() / 2
-        });
+        swipeGuide(1);
     }).on("swiperight", function(){
-        $navSpan.eq(0).addClass("active").siblings().removeClass("active");
-        $guideUl.css({
-            left: 0
+        swipeGuide(0);
+    });
+
+    $("#go-top").on("tap", function(){
+        if($(window).scrollTop() <= 1){
+            return false;
+        }
+        $("body").animate({
+            scrollTop : 0
+        }, 500);
+        var top = $(this).css("top");
+        $(this).animate({
+            top: 0
+        }, 500, function(){
+            $(this).css("top", top);
         });
+    });
+
+    $("#menu li").on("tap", function(){
+        location.href = "detail.html";
     });
 
 
 
+    function swipeGuide(index){
+        $navSpan.eq(index).addClass("active").siblings().removeClass("active");
+        $guideUl.css({
+            left: -index * $guideUl.width() / 2
+        });
+    }
 });
+
 
 /*$(document).ready(function(){
 
