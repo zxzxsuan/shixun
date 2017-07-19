ab6d0d031bf13e330ffa3cf9983e7d2ca719a9f5
zxzxsuan <13694672173@163.com>
2017-07-19 16:11:26 +0800

    7.19

--- meituanci/application/models/Product_model.php
+++ meituanci/application/models/Product_model.php
@@ -4,20 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Product_model extends CI_Model
 {
     public function get_product(){
-        //.....
-//        $sql = 'select * from t_product';
-
-//        $query = $this->db->get('t_product');
-//        return $query->result();
 
         $sql = "select t_product.*,(select sum(num) num from t_order where t_order.product_id = t_product.product_id) num from t_product";
         return $this->db->query($sql)->result();
     }
 
     public function get_product_by_id($product_id){
-//        $query = $this->db->get_where('t_product',array('product_id'=>$product_id));
-//        return $query->row();
-//
+
         $this->db->select('pro.*,bus.*');
         $this->db->from('t_product pro');
         $this->db->join('t_business bus', 'pro.business_id = bus.business_id');
@@ -37,8 +30,7 @@ class Product_model extends CI_Model
     }
 
     public function get_collect($user_id,$product_id){
-//        $this->db->get_where('t_collect',array('user_id'=>$user_id,'product_id'=>$product_id,'is_delete'=>0));
-//        return $this->db->get()->row();
+
 
         $sql = "select * from t_collect where user_id=$user_id and product_id =$product_id and is_delete = 0";
         return $this->db->query($sql)->row();
